18ec7d8a95a417d3d6afdb3f6a5534d00c3919dc
Fabrizio-shipped-it <fabrialtm3@gmail.com>
2024-11-12 17:18:37 -0300

    Update 02_ImportaciónDeArchivos -- Proyecto Janus.sql

--- 02_ImportaciónDeArchivos -- Proyecto Janus.sql
+++ 02_ImportaciónDeArchivos -- Proyecto Janus.sql
@@ -1,1 +1,265 @@
-Aqui va el script de Importación
+
+use Com2900G07
+/*
+EXEC sp_configure 'show advanced options', 1;
+RECONFIGURE;
+EXEC sp_configure 'Ad Hoc Distributed Queries', 1;
+RECONFIGURE;
+GO
+*/
+select * from level1.sucursal
+EXEC level1.importarSucursal N'TU DIRECTORIO';
+------------------------- INSERCION ----------------------------
+-- A continuación se crea las tablas para la creación de los SP que se usaran para la manipulación de tablas
+CREATE OR ALTER PROCEDURE level1.importarSucursal 
+    @rutaArchivo NVARCHAR(255) -- Parámetro para la ruta del archivo
+AS
+BEGIN
+    -- Crear tabla temporal para almacenar los datos importados
+    CREATE TABLE #tempSuc (
+        Ciudad VARCHAR(50),
+        Localidad VARCHAR(50),
+        Direccion VARCHAR(100),
+        Telefono VARCHAR(50)
+    );
+    
+    -- Declarar una variable para la consulta dinámica
+    DECLARE @sql NVARCHAR(MAX);
+
+    -- Construir la consulta dinámica
+    SET @sql = N'
+        INSERT INTO #tempSuc (Ciudad, Localidad, Direccion, Telefono)
+        SELECT Ciudad, "Reemplazar por", Direccion, Telefono
+        FROM OPENROWSET(
+            ''Microsoft.ACE.OLEDB.12.0'',
+            ''Excel 12.0;Database=' + @rutaArchivo + ''',
+            ''SELECT * FROM [sucursal$]'')';
+
+		    EXEC sp_executesql @sql;
+
+		-- Hasta aca hace la importacion de datos a la tabla temporal
+		INSERT INTO level1.sucursal(ciudad, nombreSucursal, direccion, telefono)
+		SELECT 
+		t.Ciudad,
+		t.Localidad,
+		t.Direccion,
+		t.Telefono
+		FROM #tempSuc t
+		LEFT JOIN level1.sucursal p ON t.Ciudad = p.ciudad and t.Localidad = p.nombreSucursal
+		WHERE p.ciudad IS NULL;
+		--Hasta aca hace la importacion de unicamente los que no estan incluidos en nuestra tabla de sucursales
+
+		drop table #tempSuc
+
+END;
+GO
+-----------------------
+
+CREATE OR ALTER PROCEDURE level2.importarEmpleado
+AS
+BEGIN
+		CREATE TABLE #tempEmpleado(
+		id_empleado INT,
+		nombre VARCHAR(50),
+		apellido VARCHAR(50),
+		dni INT,
+		direccion VARCHAR(100),
+		emailEmpresa VARCHAR(100),
+		emailPersonal VARCHAR(100),
+		cuil VARCHAR(15),
+		cargo VARCHAR(25),
+		sucursal VARCHAR(50),
+		turno VARCHAR(5));
+
+		INSERT INTO #tempEmpleado(id_empleado, nombre, apellido, dni, direccion, emailEmpresa, emailPersonal, cuil, cargo, sucursal, turno)
+		SELECT "Legajo/ID", Nombre, Apellido, DNI, Direccion,
+		"email personal", "email empresa", CUIL, Cargo,
+		Sucursal, replace(Turno, 'Jornada completa', 'FULL') AS Turno
+		FROM OPENROWSET('Microsoft.ACE.OLEDB.12.0',
+		'Excel 12.0;Database=TU DIRECTORIO', 
+		'SELECT * FROM [Empleados$]')
+		WHERE "Legajo/ID" is not null
+		-- Hasta aca hace la importacion de datos a la tabla temporal
+
+		INSERT INTO level2.empleado(nombre, apellido, dni, direccion, emailEmpresa, emailPersonal, cuil, cargo, sucursal, turno)
+		SELECT 
+		t.nombre, t.apellido, t.dni, t.direccion, t.emailEmpresa, t.emailPersonal, t.cuil, t.cargo, t.sucursal, t.turno
+		FROM #tempEmpleado t
+		--Hasta aca hace la importacion de unicamente los que no estan incluidos en nuestra tabla de empleados
+
+		drop table #tempEmpleado
+END;
+GO
+EXEC level2.importarEmpleado
+select * from level2.empleado
+
+------------------------- IMPORTACIÓN ----------------------------
+--- ---------------------------------------------- CATALOGO.CSV
+CREATE OR ALTER PROCEDURE level1.ImportarCatalogo @RutaArchivo NVARCHAR(270)
+AS
+BEGIN
+		DECLARE @Consulta NVARCHAR(MAX)
+		CREATE TABLE #tempCatalogo (
+		  id INT,
+		  category VARCHAR(50),
+		  name VARCHAR(100),
+		  price DECIMAL(10,2),
+		  reference_price DECIMAL(10,2),
+		  reference_unit VARCHAR(10),
+		  date VARCHAR(50));
+
+		  SET @Consulta = N'
+        BULK INSERT #tempCatalogo
+        FROM ''' + @RutaArchivo + '''
+        WITH (
+            FORMAT = ''CSV'',
+            FIELDTERMINATOR = '','',
+            ROWTERMINATOR = ''0x0a'',
+            FIRSTROW = 2,
+            CODEPAGE = ''65001'');'
+
+		EXEC sp_executesql @Consulta
+	-- hasta aca cargamos en la tabla TEMPORAL
+	INSERT INTO level1.producto(Categoria, nombreProducto, precio, ReferenciaUnidad)
+	SELECT 
+    t.category,
+    t.name,
+    t.reference_price,
+    t.reference_unit
+	FROM #tempCatalogo t
+	LEFT JOIN level1.producto p ON t.name = p.nombreProducto
+	WHERE p.nombreProducto IS NULL;  -- Solo selecciona los que no existen en la tabla permanente
+    DROP TABLE #tempCatalogo
+END
+GO
+
+EXEC level1.ImportarCatalogo N'TU DIRECTORIO';
+	select * from level1.producto
+	truncate table level1.producto
+
+
+--- ----------------------------------------------Productos_importados.xlsx
+CREATE OR ALTER PROCEDURE level1.ImportarProdImportados @RutaArchivo NVARCHAR(270)
+AS
+BEGIN
+		DECLARE @Consulta NVARCHAR(MAX)
+		CREATE TABLE #tempImportados(
+			IdProducto INT,
+			NombreProducto VARCHAR(100),
+			Proveedor VARCHAR(100),
+			Categoría VARCHAR(100),
+			CantidadPorUnidad VARCHAR(100),
+			PrecioUnidad DECIMAL(10,2));
+		
+		SET @Consulta = N'
+        INSERT INTO #tempImportados
+        SELECT *
+        FROM OPENROWSET(
+            ''Microsoft.ACE.OLEDB.12.0'',
+            ''Excel 12.0;Database=' + @RutaArchivo + ''',''SELECT * FROM [Listado de Productos$]'');';
+    EXEC sp_executesql @Consulta
+	-- Hasta aca hace la importacion de datos a la tabla temporal
+	INSERT INTO level1.producto(Categoria, nombreProducto, Precio, ReferenciaUnidad)
+	SELECT 
+    t.Categoría,
+	t.NombreProducto,
+	t.PrecioUnidad,
+	t.CantidadPorUnidad
+	FROM #tempImportados t
+	LEFT JOIN level1.producto p ON t.NombreProducto = p.nombreProducto
+	WHERE p.nombreProducto IS NULL;
+	--Hasta aca hace la importacion de unicamente los que no estan incluidos en nuestra tabla de productos
+
+	DROP TABLE #tempImportados
+END;
+GO
+
+EXEC level1.ImportarProdImportados N'TU DIRECTORIO';
+	select * from level1.producto
+	truncate table level1.producto
+--- ----------------------------------------------Electronic accessories.xlsx
+CREATE OR ALTER PROCEDURE level1.ImportarElectronicos @RutaArchivo NVARCHAR(270)	
+AS
+BEGIN
+    -- Declaración de la consulta dinámica
+    DECLARE @Consulta NVARCHAR(MAX);
+
+    -- Crear la tabla temporal para almacenar los datos importados
+    CREATE TABLE #tempElectronicos(
+        Producto VARCHAR(100),
+        Precio DECIMAL(10,2)
+    );
+
+    -- Construcción de la consulta dinámica para realizar el INSERT a la tabla temporal
+		SET @Consulta = N'
+     INSERT INTO #tempElectronicos
+        SELECT *
+        FROM OPENROWSET(
+            ''Microsoft.ACE.OLEDB.12.0'',
+            ''Excel 12.0;Database=' + @RutaArchivo + ''',''SELECT * FROM [Sheet1$]'');';
+
+    -- Ejecución de la consulta dinámica
+    EXEC sp_executesql @Consulta;
+
+    -- Inserción en la tabla final (productos), únicamente para productos nuevos
+    INSERT INTO level1.producto(Categoria, ReferenciaUnidad, nombreProducto, Precio)
+    SELECT
+        'Accesorios Electronicos' AS Categoria,
+        'ud' AS ReferenciaUnidad,
+        t.Producto,
+        t.Precio
+    FROM #tempElectronicos t
+    LEFT JOIN level1.producto p ON t.Producto = p.nombreProducto
+    WHERE p.nombreProducto IS NULL;
+
+    -- Limpieza de la tabla temporal
+    DROP TABLE #tempElectronicos;
+END;
+GO
+
+EXEC level1.ImportarElectronicos N'TU DIRECTORIO';
+	select * from level1.producto
+	truncate table level1.producto
+
+--- ----------------------------------------------Ventas_registradas.csv		----FALTA MODIFICARLA
+CREATE OR ALTER PROCEDURE level2.InsertarVentasRegistradas
+AS
+BEGIN 
+	CREATE TABLE #tempVenta(
+    "ID Factura" VARCHAR(20),
+    "Tipo de Factura" VARCHAR(1),
+    Ciudad VARCHAR(50),
+    "Tipo de cliente" VARCHAR(40),
+    Genero VARCHAR(10),
+    Producto NVARCHAR(100),
+    "Precio Unitario" DECIMAL(10,2),
+	Cantidad INT,
+	Fecha date,
+	hora time,
+	"Medio de Pago" VARCHAR(25),
+	Empleado INT,
+	"Identificador de pago" VARCHAR(50));
+
+	BULK INSERT	#tempVenta
+	FROM 'TU DIRECTORIO'
+	WITH( FORMAT= 'CSV',
+		FIELDTERMINATOR= ';',
+		ROWTERMINATOR= '0x0a',
+		FIRSTROW=2,
+		CODEPAGE='65001');
+	----------------------------------------------
+	INSERT INTO level2.VentaRegistrada(ID_Factura, Tipo_Factura, Ciudad, Tipo_Cliente, Genero, Producto, PrecioUnitario,
+									Cantidad, FechaHora, MedioPago, Empleado)
+	select t.[ID Factura], t.[Tipo de Factura], t.Ciudad, t.[Tipo de cliente], t.Genero, t.Producto, t.[Precio Unitario],
+			t.Cantidad, CAST(t.Fecha AS DATETIME) + CAST(t.hora AS DATETIME) AS FechaHoraCompleta, t.[Medio de Pago],
+				t.Empleado FROM #tempVenta t
+	
+	UPDATE v
+	SET v.Categoria = p.Categoria
+	FROM level2.VentaRegistrada AS v
+	JOIN level1.productos AS p ON v.Producto = p.NombreProd
+	WHERE v.Categoria IS NULL;
+	
+	DROP TABLE #tempVenta
+END;
+GO
