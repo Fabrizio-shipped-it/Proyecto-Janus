639b89bbc6887de9f8e7fe4bfcff05450b796011
Santiago Manghi Scheck <112188622+Santiago935@users.noreply.github.com>
2024-11-22 15:12:49 -0300

    Pruebas actualizadas a la actualizacion de SP

--- Lote de Pruebas I -- Proyecto Janus.sql
+++ Lote de Pruebas I -- Proyecto Janus.sql
@@ -34,19 +34,22 @@ EXEC level1.insertarUnSucursal 'Tatooine', 'Palacio Hutt', 'arena de Oasis entre
 go
 EXEC level1.insertarUnSucursal 'Kamino', '', 'Plataforma 115 en el medio del oceano', '1234-5678'
 go
---Resultado esperado: Solo debe existir la primera insersion
+--Resultado esperado: 
+
+-- 1    Tatooine    Palacio Hutt     arena de Oasis entre Mos Esly y Mos Espa    1111-1111  20-22222222-3
 
 SELECT * FROM level1.sucursal 
 
 
 -->Modificar
 
-EXEC level1.modificarSucursal 'Tatooine', 'Palacio Hutt', 'Mar de Dunas del Norte', '2222-2222'
+EXEC level1.modificarSucursal 1, 'Tatooine', 'Palacio Hutt', 'Mar de Dunas del Norte', '2222-2222', '20-22222222-3'
 go
-EXEC level1.modificarSucursal  'Tatooine', 'Palacio Hotter', 'Mar de Dunas del Norte', '2223-2222'
+EXEC level1.modificarSucursal  2, 'Tatooine', 'Palacio Hotter', 'Mar de Dunas del Norte', '2223-2222', '20-22222222-3'
 go
 
---Resultado esperado: Solo se modifica el primer valor
+--Resultado esperado: 
+-- 1    Tatooine    Palacio Hutt     Mar de Dunas del Norte   2222-2222  20-22222222-3
 
 SELECT * FROM level1.sucursal
 
@@ -57,9 +60,13 @@ SELECT * FROM level1.sucursal
 EXEC level1.borrarSucursal 'Palacio Hutt'
 EXEC level1.borrarSucursal 'Templo Jedi'
 
---Resultado esperado: no haya errores al eliminarlo
+--Resultado esperado: 
+
+-- 
 
 SELECT * FROM level1.sucursal
+
+
 
 -----------------------------------------CARGOS-------------------------------------------------
 
@@ -67,23 +74,35 @@ SELECT * FROM level1.sucursal
 
 EXEC level2.insertarCargo 4, 'Cientifico'
 GO
-EXEC level2.insertarCargo 2, 'Supervisor'
+EXEC level2.insertarCargo 1, 'Supervisor'
 GO
 EXEC level2.insertarCargo 1, 'Cientifico'
+GO
+EXEC level2.insertarCargo 5, 'Supervisor'
 GO
 
 SELECT * FROM level2.cargo
 
---Resultado esperado: solo debe haber un rol de supervisor (el existente predeterminado) y debe agregarse el cientifico
+--Resultado esperado: 
+
+-- 4    Cientifico
+-- 1    Supervisor
 
 -->Borrar
 
+EXEC level2.borrarCargo 2
+go
+EXEC level2.borrarCargo 3
+go
 EXEC level2.borrarCargo 4
 go
 
 SELECT * FROM level2.cargo
 
---Resultado Esperado: elimina al cientifico de la tabla
+--Resultado Esperado: 
+
+--
+
 
 -----------------------------------------EMPLEADOS---------------------------------------------
 
@@ -92,75 +111,109 @@ SELECT * FROM level2.cargo
 
 EXEC level1.insertarUnSucursal 'Tatooine', 'Palacio Hutt', 'Mar de Dunas del Norte', '1111-1111'
 go
-
+EXEC level2.insertarCargo 4, 'Cientifico'
+GO
 
 -->Insertar
 
-EXEC level2.insertarUnEmpleado 257935, 'Edward', 'Richtofen', 90453233, 'Instalaciones de Der Riese', 'dereisendrache@grupo935.com', 'erichtofen@grupo935.com', '', 'Supervisor', 'Palacio Hutt', 'TM';
+EXEC level2.insertarUnEmpleado  'Edward', 'Richtofen', 90453233, 'Instalaciones de Der Riese', 'dereisendrache@grupo935.com', 'erichtofen@grupo935.com', '', 'Cientifico', 'Palacio Hutt', 'TM';
 go
-EXEC level2.insertarUnEmpleado 89, 'Samantha', 'Maxis', 111151115, 'Casa en Agatha', '', '', '', 'Stoomtroper', 'Palacio Hutt', 'TN';
+EXEC level2.insertarUnEmpleado  'Samantha', 'Maxis', 111151115, 'Casa en Agatha', '', '', '', 'Stoomtroper', 'Palacio Hutt', 'TN';
 go
-EXEC level2.insertarUnEmpleado 257115, 'Ledwing', 'Maxis', 12, 'Kino der Toten', 'dereisendrache@grupo935.com', 'lmaxis@grupo935.com', '', 'Cajero', 'Placio Hutt', 'TM';
+EXEC level2.insertarUnEmpleado  'Ledwing', 'Maxis', 12, 'Kino der Toten', 'dereisendrache@grupo935.com', 'lmaxis@grupo935.com', '', 'Cajero', 'Placio Hutt', 'TM';
 go
-EXEC level2.insertarUnEmpleado 257115, 'Ledwing', 'Maxis', 12222222, 'Kino der Toten', 'dereisendrache@grupo935.com', 'lmaxis@grupo935.com', '', 'Comandante', 'Palacio Hutt', 'TM';
+EXEC level2.insertarUnEmpleado  'Ledwing', 'Maxis', 12222222, 'Kino der Toten', 'dereisendrache@grupo935.com', 'lmaxis@grupo935.com', '', 'Comandante', 'Palacio Hutt', 'TM';
 go
-EXEC level2.insertarUnEmpleado 257115, 'Ledwing', 'Maxis', 12222222, 'Kino der Toten', 'dereisendrache@grupo935.com', 'lmaxis@grupo935.com', '', 'Cajero', 'Palacio Hutt', 'TM';
+EXEC level2.insertarUnEmpleado  'Ledwing', 'Maxis', 12222222, 'Kino der Toten', 'dereisendrache@grupo935.com', 'lmaxis@grupo935.com', '', 'Cientifico', 'Palacio Hutt', 'TM';
 go
 
 
 
---Resultado esperado: solo debe existir la primera insercion y la ultima
+--Resultado esperado: 
+
+-- Edward   Richtofen   90453233    Instalaciones de Der Riese  dereisendrache@grupo935.com     erichtofen@grupo935.com  '' 
+-- Cientifico   Palacio Hutt     TM 1
+
+-- Ledwing   Maxis  12222222    Kino der Toten  dereisendrache@grupo935.com     lmaxis@grupo935.com  ''
+--  Cientifico  Palacio Hutt    TM  1
 
 SELECT * FROM level2.empleado 
 go
 
 -->Modificado
-EXEC level2.modificarEmpleado 257935, 'Edward', 'Richtofen',  'Instalaciones del Puesto Griffin', 'dereisendrache@grupo935.com', 'erichtofen@grupo935.com', 2, 1, 2, 'TN'
+EXEC level2.modificarEmpleado 257020, 'Edward', 'Richtofen',  'Instalaciones del Puesto Griffin', 'dereisendrache@grupo935.com', 'erichtofen@grupo935.com', 2, 'Cientifico', 'Palacio Hutt', 'TN'
 go
-EXEC level2.modificarEmpleado 257935, 'Edward', 'Richtofen',  'Instalaciones del Puesto Griffin', 'dereisendrache@grupo935.com', 'erichtofen@grupo935.com', 2, 'Cajero', 'Palacio Hutt', 'TM'
+EXEC level2.modificarEmpleado 257022, 'Edward', 'Richtofen',  'Instalaciones del Puesto Griffin', 'dereisendrache@grupo935.com', 'erichtofen@grupo935.com', 2, 'Cajero', 'Palacio Hutt', 'TM'
 go
 EXEC level2.modificarEmpleado 935, 'Edwara', 'Richtofenia',  'Instalaciones de Shino Numa', 'dereisendrache@grupo935.com', 'erichtofen@grupo935.com', '', 1, 1, 'TN'
 go
 
--- Resultado esperado: Richtofen esta en el Puesto Griffin, con cuil 2, cargo cajero y en el turno TN.
+-- Resultado esperado
+
+-- Edward   Richtofen   90453233    Instalaciones del Puesto Griffin  dereisendrache@grupo935.com     erichtofen@grupo935.com  2 
+-- Cientifico   Palacio Hutt     TM 1
+
+-- Ledwing   Maxis  12222222    Kino der Toten  dereisendrache@grupo935.com     lmaxis@grupo935.com  ''
+--  Cientifico  Palacio Hutt    TM  1
 
 SELECT * FROM level2.empleado 
 go
---->Borrado
 
-EXEC level2.borrarEmpleado 257935
+--->Dar de baja al empleado
+
+EXEC level2.bajarEmpleado 257020
 go
-EXEC level2.borrarEmpleado 257115
+EXEC level2.bajarEmpleado 257021
 go
-EXEC level2.borrarEmpleado 4
+EXEC level2.bajarEmpleado 4
 go
 SELECT * FROM level2.empleado
 go
 
---Resultado esperado: solo deberia borrar los tests
+--Resultado esperado: 
+-- Edward   Richtofen   90453233    Instalaciones del Puesto Griffin  dereisendrache@grupo935.com     erichtofen@grupo935.com  2 
+-- Cientifico   Palacio Hutt     TM     0
 
+-- Ledwing   Maxis  12222222    Kino der Toten  dereisendrache@grupo935.com     lmaxis@grupo935.com  ''
+--  Cientifico  Palacio Hutt    TM      0
 
-EXEC level2.reactivarEmpleado 257935
+-->Reactivar empleado
+
+EXEC level2.reactivarEmpleado 257020
 SELECT * FROM level2.empleado
 go
+
+--Resultado esperado: 
+-- Edward   Richtofen   90453233    Instalaciones del Puesto Griffin  dereisendrache@grupo935.com     erichtofen@grupo935.com  2 
+-- Cientifico   Palacio Hutt     TM     1
+
+-- Ledwing   Maxis  12222222    Kino der Toten  dereisendrache@grupo935.com     lmaxis@grupo935.com  ''
+--  Cientifico  Palacio Hutt    TM      0
+EXEC level2.bajarEmpleado 257020
+go
+
 
 --------------------------------PRODUCTO-------------------------------------------
 
 -->Insertar
 
-EXEC level1.insertarUnProducto 'Cubo de compania', 'Aperture Science', 43.3
+EXEC level1.insertarUnProducto 'Cubo de compania', 'Aperture Science', 43.3, 'unidad'
 GO
-EXEC level1.insertarUnProducto 'Torreta', 'Half Life',  115.935
+EXEC level1.insertarUnProducto 'Torreta', 'Black Mesa',  115.935, 'unidad'
 GO
-EXEC level1.insertarUnProducto  'Cubo de compania', 'Aperture Science', 43.3
+EXEC level1.insertarUnProducto  'Cubo de compania', 'Aperture Science', 43.3, 'unidad'
 GO
-EXEC level1.insertarUnProducto 'Cubo laser','Imperio Klingon',  89
+EXEC level1.insertarUnProducto 'Cubo laser','Imperio Klingon',  89, 'klingons'
 GO
-EXEC level1.insertarUnProducto  'GLaDOS', 'Aperture Science', -2323
+EXEC level1.insertarUnProducto  'GLaDOS', 'Aperture Science', -2323, 'kilos'
 GO
 
 
---Resultado esperado: Solo debe existir la torreta, el cubo de compania y el cubo laser
+--Resultado esperado: 
+-- 1    Aperture Science    Cubo de Compania    43.30   unidad  1
+-- 2    Black Mesa  115.935 unidad  1
+-- 2    Imperio Klingon Cubo laser  89  klingons    1
+
 SELECT * FROM level1.producto
 
 
@@ -173,22 +226,52 @@ GO
 EXEC level1.modificarProducto 1, 'Aperture Science',  -34
 GO
 
---Resultado esperado: Solo los dos primeros tienen efecto
+--Resultado esperado:
+-- 1    Aperture Science    Cubo de Compania    78.90   unidad  1
+-- 2    Black Mesa  935.2 unidad  1
+-- 3    Imperio Klingon Cubo laser  89  klingons    1
+
 
 SELECT * FROM level1.producto
 go
--->Borrado
 
-EXEC level1.borrarProducto 2
-GO
-EXEC level1.borrarProducto 1
-GO
-EXEC level1.borrarProducto 3
-GO
+-->Dar de baja Producto
 
---Resultado esperado: borra los tests
+EXEC level1.bajarProducto 1
+go
+EXEC level1.bajarProducto 2
+go
+
+
+--Resultado esperado:
+-- 1    Aperture Science    Cubo de Compania    78.90   unidad  0
+-- 2    Black Mesa  935.2 unidad  0
+-- 3    Imperio Klingon Cubo laser  89  klingons    1
 
 SELECT * FROM level1.producto
 go
+
+-->Dar de alta producto
+
+EXEC level1.reactivarProducto 1
+go
+
+--Resultado esperado:
+-- 1    Aperture Science    Cubo de Compania    78.90   unidad  1
+-- 2    Black Mesa  935.2 unidad  0
+-- 3    Imperio Klingon Cubo laser  89  klingons    1
+
+SELECT * FROM level1.producto
+go
+
+
+EXEC level1.bajarProducto 1
+go
+EXEC level1.bajarProducto 2
+go
+EXEC level1.bajarProducto 3
+go
+
+
 
 
