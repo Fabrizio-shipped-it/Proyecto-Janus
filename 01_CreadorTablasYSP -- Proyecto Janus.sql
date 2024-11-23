bf9c002863404e8cad7855d378fdb2c522ef6cbb
María Celeste Torres Morán <mariacelestetorres2001@gmail.com>
2024-11-22 22:55:26 -0300

    Update 01_CreadorTablasYSP -- Proyecto Janus.sql

--- 01_CreadorTablasYSP -- Proyecto Janus.sql
+++ 01_CreadorTablasYSP -- Proyecto Janus.sql
@@ -108,7 +108,8 @@ GO
 IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'producto')
 BEGIN
 	CREATE TABLE level1.producto(			idProducto INT PRIMARY KEY IDENTITY(1,1),	 	
-							categoria VARCHAR(50) NOT NULL,			
+							categoria VARCHAR(50) NOT NULL,	
+							idProducto INT REFERENCES level1.producto(idProducto),
 							nombreProducto VARCHAR (100) NOT NULL,			
 							precio DECIMAL(10,2) NOT NULL,
 							referenciaUnidad VARCHAR(30),
