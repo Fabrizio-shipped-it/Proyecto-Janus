5a6b412bc250745dd8383902b6142f4415dbe310
Santiago Manghi Scheck <112188622+Santiago935@users.noreply.github.com>
2024-11-26 16:46:28 -0300

    Mas reportes

--- 03_Reportes.sql
+++ 03_Reportes.sql
@@ -63,8 +63,68 @@ END
 go
 
 
+
+--Reporte con los cinco productos mas vendidos de la fecha
+
+CREATE OR ALTER PROCEDURE level2.reporteCincoProdMasVendidos @mes INT, @anio INT AS
+    BEGIN
+    
+        SELECT DATEPART(WEEK, f.fechaHora) AS Semana,
+               p.nombreProducto,
+               SUM(d.cantidad) AS CantidadVendida
+        FROM level2.detalleVenta d
+        INNER JOIN level1.producto p ON d.idProducto = p.idProducto
+        INNER JOIN level2.factura f ON f.iD_Venta = d.iD_Venta
+        WHERE MONTH(f.fechaHora) = @Mes AND YEAR(f.fechaHora) = @Anio
+        GROUP BY DATEPART(WEEK, f.fechaHora), p.nombreProducto
+        ORDER BY Semana, CantidadVendida DESC OFFSET 0 ROWS FETCH NEXT 5 ROWS ONLY
+        FOR XML AUTO, ELEMENTS
+        
+
+    END
+go
+
+-- Reporte de los cinco productos menos vendidos por fecha
+
+CREATE OR ALTER PROCEDURE level2.reporteCincoProductosMenosVendidos @mes INT, @anio INT AS
+    BEGIN
+        SELECT p.nombreProducto,
+               SUM(dv.cantidad) AS CantidadVendida
+        FROM level2.detalleVenta dv
+        INNER JOIN level1.producto p ON dv.idProducto = p.idProducto
+        INNER JOIN level2.factura f ON f.iD_Venta = dv.iD_Venta
+        WHERE MONTH(f.fechaHora) = @Mes AND YEAR(f.fechaHora) = @Anio
+        GROUP BY p.nombreProducto
+        ORDER BY CantidadVendida ASC
+        OFFSET 0 ROWS FETCH NEXT 5 ROWS ONLY
+        FOR XML AUTO, ELEMENTS
+    END
+go
+
+--Reporte total de venta acumulados por sucursal en una fecha
+
+CREATE OR ALTER PROCEDURE level2.reporteTotalAcumuloVentas @fecha DATETIME, @idSucursal INT AS
+    BEGIN
+        SELECT f.iD_Factura,
+               f.fechaHora,
+               s.nombreSucursal,
+               SUM(dv.cantidad * dv.precioUnitario) AS TotalFactura
+        FROM level2.factura f
+        INNER JOIN level2.detalleVenta dv ON f.iD_Venta = dv.iD_Venta
+        INNER JOIN level1.sucursal s ON f.iD_Sucursal = s.idSucursal
+        WHERE CAST(f.fechaHora AS DATE) = @Fecha AND s.idSucursal = @idSucursal
+        GROUP BY f.iD_Factura, f.fechaHora, s.nombreSucursal
+        FOR XML AUTO, ELEMENTS
+    END
+go
+
 EXEC level2.reporteFehaProdVendidosPorSucursal '2023-11-26', '2024-12-25'
+go
 EXEC level2.reporteFechaCantidadProductos '2023-11-26', '2024-12-25'
+go
+EXEC level2.reporteCincoProdMasVendidos 11, 2024
+EXEC level2.reporteCincoProductosMenosVendidos 11, 2024
+EXEC level2.reporteTotalAcumuloVentas '2024-11-26', 2
 
 
 CREATE OR ALTER PROCEDURE level2.TotalFacturadoPorDiaSemana
